16d65b1bb865d80d4403c01cc3f6211a4c36cb61
agata-nowicka <aggat.nowicka@gmail.com>
2021-03-04 21:08:44 +0100

    add styles and grid to SearchBar

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,6 @@ import { Grid } from '@material-ui/core';
 const Home = () => {
   return (
     <Grid container direction="column" alignItems="center" justify="center">
-      <h1> Search Bar </h1>
       <SearchBar />
     </Grid>
   );
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -4,15 +4,45 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Results from './Results';
 import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import SearchIcon from '@material-ui/icons/Search';
 import { InputAdornment } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Avatar from '@material-ui/core/Avatar';
+
+const useStyles = makeStyles((theme) => ({
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 function SearchBar() {
   const [searchInput, setSearchInput] = useState('');
   const [repos, setRepos] = useState([]);
   const [searchedUser, setSearchedUser] = useState(null);
+  const classes = useStyles();
 
   const handleChange = (e) => {
     setSearchInput(e.target.value);
@@ -34,44 +64,66 @@ function SearchBar() {
   const handleClear = () => {
     setSearchInput('');
     setRepos([]);
+    setSearchedUser(null);
   };
 
   return (
-    <>
-      <div>
-        <GitHubIcon />
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <GitHubIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Search for a GitHub user
+        </Typography>
         <form noValidate autoComplete="off" onSubmit={handleSearch}>
-          <TextField
-            id="outlined-basic"
-            label="User name"
-            variant="outlined"
-            type="text"
-            value={searchInput}
-            onChange={handleChange}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <SearchIcon />
-                </InputAdornment>
-              ),
-            }}
-          />
-          <Button variant="contained" color="secondary" onClick={handleSearch} type="submit">
-            Search
-          </Button>
-          <Link
-            to={{
-              pathname: '/',
-            }}
-          >
-            <Button variant="contained" onClick={handleClear}>
-              Clear
-            </Button>
-          </Link>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                id="outlined-basic"
+                label="User name"
+                variant="outlined"
+                fullWidth
+                type="text"
+                value={searchInput}
+                onChange={handleChange}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <SearchIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+          </Grid>
+
+          <div className={classes.heroButtons}>
+            <Grid container spacing={2} justify="center">
+              <Grid item>
+                <Button variant="outlined" color="secondary" onClick={handleSearch} type="submit">
+                  Search
+                </Button>
+              </Grid>
+              <Grid item>
+                <Link
+                  to={{
+                    pathname: '/',
+                  }}
+                >
+                  <Button variant="contained" onClick={handleClear}>
+                    Clear
+                  </Button>
+                </Link>
+              </Grid>
+            </Grid>
+          </div>
         </form>
       </div>
+
       <Results repos={repos} user={searchedUser} />
-    </>
+    </Container>
   );
 }
 
