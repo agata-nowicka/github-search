e22d137a27e456bb8c93edff896788341d91358c
agata-nowicka <aggat.nowicka@gmail.com>
2021-03-11 21:12:53 +0100

    Add back button to Repo, change Repo's design

--- src/Components/Repo.js
+++ src/Components/Repo.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -10,31 +10,43 @@ import CardActions from '@material-ui/core/CardActions';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: 345,
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    maxWidth: '80%',
+    margin: 'auto',
+    transition: '0.3s',
+    boxShadow: '0 8px 40px -12px rgba(0,0,0,0.3)',
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
   },
   media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
+    width: 200,
   },
   avatar: {
-    backgroundColor: red[500],
+    backgroundColor: theme.palette.primary.main,
+  },
+  button: {
+    marginTop: theme.spacing(6),
+
+    backgroundColor: theme.palette.primary.main,
+  },
+  center: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 }));
 
@@ -56,38 +68,48 @@ const Repo = () => {
   }, []);
 
   return (
-    <Card className={classes.root}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label="recipe" className={classes.avatar}>
-            {author[0]}
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title={name}
-        subheader="Lorem ipsum"
-      />
-      <CardMedia className={classes.media} image={avatar} title="avatar" />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          Choosen repo is {name}. The author is {author}
-          <br />
-          Used languages are: {languages.join(', ')}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-      </CardActions>
-    </Card>
+    <Container className={classes.center} component="main" maxWidth="m">
+      <CssBaseline />
+      <Card className={classes.root}>
+        <div className={classes.details}>
+          <CardContent className={classes.content}>
+            <CardHeader
+              avatar={
+                <Avatar aria-label="recipe" className={classes.avatar}>
+                  {author[0]}
+                </Avatar>
+              }
+              title={name}
+              subheader="Lorem ipsum"
+            />
+
+            <Typography variant="body2" color="textSecondary" component="p">
+              Choosen repo is {name}. The author is {author}
+              <br />
+              Used languages are: {languages.join(', ')}
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="share">
+              <ShareIcon />
+            </IconButton>
+          </CardActions>
+        </div>
+        <CardMedia className={classes.media} image={avatar} title="avatar" />
+      </Card>
+      <Link
+        to={{
+          pathname: '/',
+        }}
+      >
+        <Button variant="contained" className={classes.button}>
+          Back
+        </Button>
+      </Link>
+    </Container>
   );
 };
 
