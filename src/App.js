d0d8de63ab18190a645de6c175d0c9e5c2b8b795
agata-nowicka <aggat.nowicka@gmail.com>
2021-03-24 19:25:07 +0100

    browserouter2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import theme from './styles/theme';
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={window.location.pathname || ''}>
         <Switch>
           <Route exact path={PATHS.HOME} component={Home} />
           <Route path={PATHS.REPO} component={Repo} />
