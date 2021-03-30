edb84db3144e7df68941ccd0ff6592beff958540
Agata <47263506+agata-nowicka@users.noreply.github.com>
2021-03-30 20:49:04 +0200

    Update Repo.js
    handle 'no languages'

--- src/Components/Repo.js
+++ src/Components/Repo.js
@@ -25,12 +25,9 @@ const useStyles = makeStyles((theme) => ({
     boxShadow: '0 8px 40px -12px rgba(0,0,0,0.3)',
     background: 'rgba(255, 255, 255, 0.7)',
   },
-  details: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
   content: {
     flex: '1 0 auto',
+    flexDirection: 'column',
   },
 
   media: {
@@ -65,56 +62,55 @@ const useStyles = makeStyles((theme) => ({
 const Repo = () => {
   const { author, name } = useParams();
   const [languages, setLanguages] = useState([]);
-  const [avatar, setAvatar] = useState([]);
+  const [userAvatar, setUserAvatar] = useState([]);
   const [repoUrl, setRepoUrl] = useState([]);
   const classes = useStyles();
   const history = useHistory();
-  //useCallback zwróci zapamiętaną wersję funkcji, która zmieni się tylko wtedy, gdy zmieni się któraś z zależności, czyli historia w URL
+  //useCallback retuerns memorized function versiion, which changes only when URL history changes
   const handleBack = useCallback(() => history.push(`/?query=${author}`), [history]);
   useEffect(async () => {
     try {
       const resultLang = await axios(`https://api.github.com/repos/${author}/${name}/languages`);
-      setLanguages(Object.keys(resultLang.data));
+      const languageAr = Object.keys(resultLang.data);
+      languageAr.length !== 0 ? setLanguages(languageAr) : setLanguages(['No used languages']);
       const resultAuth = await axios(`https://api.github.com/users/${author}`);
-      setAvatar(resultAuth.data.avatar_url);
+      setUserAvatar(resultAuth.data.avatar_url);
       setRepoUrl(`https://github.com/${author}/${name}`);
     } catch (error) {
       console.log(error);
     }
   }, []);
   return (
-    <Container className={classes.center} component="main" maxWidth="m">
+    <Container className={classes.center} component="main" maxWidth="md">
       <CssBaseline />
       <Card className={classes.root}>
-        <div className={classes.details}>
-          <CardContent className={classes.content}>
-            <CardHeader
-              avatar={
-                <Avatar aria-label="first letter" className={classes.avatar}>
-                  {author[0]}
-                </Avatar>
-              }
-              title={
-                <a target="blank" href={repoUrl} className={classes.customLink}>
-                  {name}
-                </a>
-              }
-            />
-            <Typography variant="body1" color="textSecondary" component="p">
-              The
-              <Box fontWeight="fontWeightBold" m={1} display="inline">
-                author
-              </Box>
-              is {author} <br />
-              Used
-              <Box fontWeight="fontWeightBold" m={1} display="inline">
-                languages
-              </Box>
-              are: {languages.join(', ')}
-            </Typography>
-          </CardContent>
-        </div>
-        <CardMedia className={classes.media} image={avatar} title="avatar" />
+        <CardContent className={classes.content}>
+          <CardHeader
+            avatar={
+              <Avatar aria-label="first letter" className={classes.avatar}>
+                {author[0]}
+              </Avatar>
+            }
+            title={
+              <a target="blank" href={repoUrl} className={classes.customLink}>
+                {name}
+              </a>
+            }
+          />
+          <Typography variant="body1" color="textSecondary" component="p">
+            The
+            <Box fontWeight="fontWeightBold" m={1} display="inline">
+              author
+            </Box>
+            is {author} <br />
+            Used
+            <Box fontWeight="fontWeightBold" m={1} display="inline">
+              languages
+            </Box>
+            are: {languages.join(', ')}
+          </Typography>
+        </CardContent>
+        <CardMedia className={classes.media} image={userAvatar} title="avatar" />
       </Card>
       <div>
         <Button variant="contained" className={classes.button} onClick={handleBack}>
